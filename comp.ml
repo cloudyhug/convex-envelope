0de65a81823390028a098e39b562547151da83c3
cloudyhug <cloudyhug@protonmail.ch>
2018-03-13 11:02:37 +0100

    organised comments on comp.ml

--- comp.ml
+++ comp.ml
@@ -11,7 +11,9 @@ let rec graph_of_list pointlist : graph =
     | [] -> []
     | point :: r -> (Point point) :: graph_of_list r
 
-(* val minimise_on_graph : (point -> 'a) -> graph -> point *)
+(* val minimise_on_graph : (point -> 'a) -> graph -> point
+ * Finds the point in the graph that minimises the function.
+ *)
 let minimise_on_graph f (graph : graph) =
     match graph with
     | [] -> failwith "minimise_on_graph - invalid argument : empty graph"
@@ -22,7 +24,9 @@ let minimise_on_graph f (graph : graph) =
             | point :: r -> if f point < f memorised_point then minimise r point else minimise r memorised_point
         in minimise r firstpoint
 
-(* val maximise_on_graph : (point -> 'a) -> graph -> point *)
+(* val maximise_on_graph : (point -> 'a) -> graph -> point
+ * Finds the point in the graph that maximises the function.
+ *)
 let maximise_on_graph f (graph : graph) =
     match graph with
     | [] -> failwith "maximise_on_graph - invalid argument : empty graph"
@@ -66,7 +70,9 @@ let create_window minx maxx miny maxy =
     Graphics.moveto 0 !originy;
     Graphics.lineto width !originy
 
-(* val open_window : graph -> unit *)
+(* val open_window : graph -> unit
+ * Creates a window that is big enough to display the graph.
+ *)
 let open_window graph =
     let minx = int_of_float (ceil (x (minimise_on_graph x graph))) - 1 in
     let miny = int_of_float (ceil (y (minimise_on_graph y graph))) - 1 in
@@ -115,15 +121,19 @@ let draw_polygon pointlist =
         draw_lines r;
         Graphics.lineto (physical_x (x firstpoint)) (physical_y (y firstpoint))
 
-(* val isleft : point -> line -> bool *)
+(* val isleft : point -> line -> bool
+ * Tells whether a point is on the left of a line.
+ *)
 let isleft p (Line(p1, p2)) =
     if p = p1 || p = p2 then false else
     let x1, x2, x3 = x p1, x p2, x p in
     let y1, y2, y3 = y p1, y p2, y p in
     (x2 -. x1) *. (y3 -. y1) -. (y2 -. y1) *. (x3 -. x1) > 0.
 
-(* TO REMOVE OR EDIT *)
-(* val mostleft : point list -> point -> point option -> point option *)
+(* val mostleft : point list -> point -> point option -> point option
+ * (Auxiliary function)
+ * Iterates on the point list and finds the farthest point on the left of the point given as a parameter.
+ *)
 let rec mostleft graph point mostleftpoint =
     match graph with
     | [] -> mostleftpoint
@@ -137,7 +147,9 @@ let rec mostleft graph point mostleftpoint =
                 mostleft r point (Some mlp)
     end
 
-(* val jarvis : graph -> point list *)
+(* val jarvis : graph -> point list
+ * Returns the graph's convex envelope, computed with the Jarvis algorithm.
+ *)
 let jarvis graph =
     let p0 = minimise_on_graph x graph in
     let rec iterate graph point envelope =
@@ -150,7 +162,10 @@ let jarvis graph =
                 iterate graph (mostleft graph p None) (p :: envelope)
     in iterate graph None []
 
-(* val quicksort_angles : graph -> point -> graph *)
+(* val quicksort_angles : graph -> point -> graph
+ * Sorts the graph depending on the angles the points make with the horizontal axis.
+ * The parameter point p0 is considered as the origin for the computation.
+ *)
 let quicksort_angles (graph : graph) p0 : graph =
     let atan2point (Point (x, y)) (Point (x0, y0)) = atan2 (y -. y0) (x -. x0) in
     let graph_with_angles = List.map (fun point -> (point, atan2point point p0)) graph in
@@ -165,7 +180,9 @@ let quicksort_angles (graph : graph) p0 : graph =
             (qsort l1) @ leq @ (qsort l2)
     in List.map fst (qsort graph_with_angles)
 
-(* val graham : graph -> point list *)
+(* val graham : graph -> point list
+ * Returns the graph's convex envelope, computed with the Jarvis algorithm.
+ *)
 let graham graph =
     let rec iterate graph stack =
         match graph with
@@ -186,31 +203,39 @@ let graham graph =
     | [] -> [p0]
     | firstpoint :: r -> iterate r [firstpoint; p0]
 
-(* val insert_after : 'a -> 'a -> 'a list -> 'a list *)
-let rec insert_after point newpoint pointlist =
-    match pointlist with
+(* val insert_after : 'a -> 'a -> 'a list -> 'a list
+ * Inserts b after a in list l.
+ *)
+let rec insert_after a b l =
+    match l with
     | [] -> failwith "insert_after - empty list"
-    | p :: r ->
-        if p = point then
-            p :: newpoint :: r
+    | e :: r ->
+        if e = a then
+            e :: b :: r
         else
-            p :: (insert_after point newpoint r)
+            e :: (insert_after a b r)
 
-(* val equation : point -> point -> float * float * float *)
+(* val equation : point -> point -> float * float * float
+ * Returns the (a,b,c) parameters of a 2-dimensional 'ax+by+c=0' line that contains both points.
+ *)
 let equation (Point (x1, y1)) (Point (x2, y2)) =
     let a = y2 -. y1 in
     let b = x1 -. x2 in
     let c = -. a *. x1 -. b *. y1 in
     (a, b, c)
 
-(* val farthestpoint : graph -> point -> point -> point *)
+(* val farthestpoint : graph -> point -> point -> point
+ * Finds the point in the graph that is the farthest one from the (a,b) line.
+ *)
 let farthestpoint graph a b =
     let distance (Line (p1, p2)) (Point (x, y)) =
         let a, b, c = equation p1 p2 in
         (abs_float (a *. x +. b *. y +. c)) /. (sqrt (a *. a +. b *. b))
     in maximise_on_graph (distance (Line (a, b))) graph
 
-(* val quickhull : graph -> point list *)
+(* val quickhull : graph -> point list
+ * Returns the graph's convex envelope, computed with the Quickhull algorithm.
+ *)
 let quickhull graph =
     let rec qhull graph a b envelope =
         let g = List.filter (fun point -> point <> a && point <> b) graph in
@@ -234,7 +259,7 @@ let quickhull graph =
 
 (*
 
-Jarvis infinite loop on this
+Jarvis makes an infinite loop on this...
 
 [(-4.73,-4.92);(2.58,1.77);(-3.17,-3.02);(-4.86,-0.70);(1.86,-4.95);(3.45,1.84);(1.33,4.56);(1.82,1.99);(-1.87,-0.11);(-3.62,1.80);(2.52,-2.80);(1.97,4.37);(-3.38,1.11);(4.33,2.51);(2.56,-4.48);(-4.41,-4.05);(2.72,2.83);(4.81,3.18);(-2.52,4.54);(4.60,4.95);(1.79,4.35);(3.91,3.05);(0.04,-1.87);(3.02,0.72);(-2.48,3.17);(-4.63,4.40);(3.77,-4.96);(1.15,2.33);(-0.16,-4.61);(-4.09,0.48);(1.43,-2.60);(-4.76,-3.50);(4.68,-0.85);(-1.31,-4.96);(4.99,-2.84);(-3.50,-1.71);(-3.66,1.78);(4.91,-4.59);(1.13,1.37);(-0.46,2.93);(2.33,3.66);(3.70,-0.09);(-2.76,1.10);(1.49,-0.15);(-4.66,2.08);(4.49,2.39);(3.89,1.77);(-4.08,4.72);(4.77,3.57);(-4.27,-0.39);(2.90,4.76);(1.54,-2.77);(-2.36,4.24);(0.61,1.45);(-0.62,3.78);(2.43,-4.85);(0.0,0.0)]
 
